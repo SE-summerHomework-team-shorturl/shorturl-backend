04286397c4403e728905117ffd842cb82e89201e
hzfsls <2508342095@qq.com>
2020-07-10 13:50:48 +0800

    Delete : demo

--- springcloud-demo/user-service/src/main/java/com/example/userservice/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.userservice.service;
-
-
-import com.example.sharedentity.dto.Message;
-import com.example.sharedentity.entity.User;
-
-public interface UserService {
-    Message register(User user);
-}
