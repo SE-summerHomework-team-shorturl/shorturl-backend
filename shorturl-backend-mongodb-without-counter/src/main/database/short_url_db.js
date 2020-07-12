0c0af2bab38f0349c79654c86ecacddf6a24c332
billsjchw <billsjc@126.com>
2020-07-12 16:59:31 +0800

    chore: remove shorturl-backend-mongodb-without-counter/

--- shorturl-backend-mongodb-without-counter/src/main/database/short_url_db.js
+++
@@ -1,9 +0,0 @@
-let conn = new Mongo('localhost:27017');
-let db = conn.getDB('short_url_db');
-db.dropDatabase();
-db = conn.getDB('short_url_db');
-
-db.createCollection('users');
-db.createCollection('short_urls');
-
-db.users.createIndex({ username: 1 }, { unique: true });
--- shorturl-backend-mongodb-without-counter/src/main/java/com/example/shorturl/service/RedirectService.java
+++
@@ -1,7 +0,0 @@
-package com.example.shorturl.service;
-
-import com.example.shorturl.entity.ShortUrl;
-
-public interface RedirectService {
-    ShortUrl findShortUrlByToken(String token) throws Exception;
-}
