7aef68036bddc840f808635703da65bff3c731d4
hzfsls <2508342095@qq.com>
2020-07-15 10:26:50 +0800

    Update : gateway filter

--- shorturl-springcloud/security-gateway/src/main/java/com/example/securitygateway/handler/UserCheckedFilter.java
+++ shorturl-springcloud/security-gateway/src/main/java/com/example/securitygateway/handler/UserCheckedFilter.java
@@ -2,8 +2,10 @@ package com.example.securitygateway.handler;
 
 import com.example.securitygateway.misc.ShortUrlUserDetails;
 import com.example.sharedentity.entity.User;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,22 +14,13 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class UserCheckedFilter implements Ordered, GlobalFilter {
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        ServerHttpRequest request = exchange.getRequest();
-        if(request.getHeaders().get("Authorization") != null) {
-            User user = ((ShortUrlUserDetails) SecurityContextHolder.getContext()
-                    .getAuthentication().getPrincipal()).getUser();
-
-            request.getQueryParams().add("userId", user.getId().toString());
-            exchange.mutate().request(request).build();
-        }
-        return chain.filter(exchange);
-    }
+public class UserCheckedFilter extends AbstractGatewayFilterFactory<Object> {
 
     @Override
-    public int getOrder() {
-        return Ordered.HIGHEST_PRECEDENCE;
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+
+            return chain.filter(exchange);
+        };
     }
 }
