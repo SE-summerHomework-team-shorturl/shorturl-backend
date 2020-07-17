bd57d76cb1e4c9cb62505d04c8a9376eac8918e7
JerryCrab <1984525794qq.com>
2020-07-17 16:18:03 +0800

    Test: Base62Encoder.java

--- shorturl-springcloud/shared-entity/src/main/java/com/example/sharedentity/util/Base62Encoder.java
+++ shorturl-springcloud/shared-entity/src/main/java/com/example/sharedentity/util/Base62Encoder.java
@@ -1,12 +1,17 @@
 package com.example.sharedentity.util;
 
 public class Base62Encoder {
+    static final public String negative_err_msg="num should be non-negative";
+    static final public String empty_token_msg="token is empty";
+    static final public String prefix_zero_msg="token contains prefix zero";
+    static final public String Invalid_token_msg="Invalid token";
+
     static public String encode(int num) throws Exception {
         if (num < 0)
-            throw new Exception("Input should be non-negative");
+            throw new IllegalArgumentException( negative_err_msg);
         StringBuilder builder = new StringBuilder();
         while (num > 0) {
-            int digit = num % 62;
+            long digit = num % 62;
             if (digit < 10)
                 builder.append((char) ('0' + digit));
             else if (digit < 36)
@@ -21,6 +26,10 @@ public class Base62Encoder {
     static public int decode(String token) throws Exception {
         int num = 0;
         int len = token.length();
+        if (len == 0)
+            throw new IllegalArgumentException(empty_token_msg);
+        if (len > 1 && token.charAt(0) == '0')
+            throw new IllegalArgumentException(prefix_zero_msg);
         for (int i = 0; i < len; ++i) {
             char c = token.charAt(i);
             num *= 62;
@@ -31,7 +40,7 @@ public class Base62Encoder {
             else if (c >= 'a' && c <= 'z')
                 num += (c - 'a') + 36;
             else
-                throw new Exception("Invalid token");
+                throw new IllegalArgumentException(Invalid_token_msg);
         }
         return num;
     }
