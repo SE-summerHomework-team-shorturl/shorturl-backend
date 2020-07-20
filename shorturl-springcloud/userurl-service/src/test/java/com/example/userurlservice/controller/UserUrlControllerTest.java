c1f0898da07d9cf90878a068c2d8f8cd135ae06f
hzfsls <2508342095@qq.com>
2020-07-20 16:50:25 +0800

    Update ：All test passed

--- shorturl-springcloud/userurl-service/src/test/java/com/example/userurlservice/controller/UserUrlControllerTest.java
+++ shorturl-springcloud/userurl-service/src/test/java/com/example/userurlservice/controller/UserUrlControllerTest.java
@@ -1,17 +1,26 @@
 package com.example.userurlservice.controller;
 
-import com.example.misc.AuthTokenTests;
+import com.example.misc.MyAuthentication;
+import com.example.misc.UrlShortenerTokenEnhancer;
 import com.example.sharedentity.dto.Message;
+import com.example.sharedentity.entity.User;
 import com.example.userurlservice.service.UserUrlService;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -20,18 +29,44 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashMap;
+import java.util.HashSet;
+
 import static org.mockito.Mockito.when;
 
 @AutoConfigureMockMvc
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
-class UserUrlControllerTest extends AuthTokenTests {
+class UserUrlControllerTest{
     @Autowired
     private MockMvc mockMvc;
     @MockBean
     private UserUrlService userUrlService;
+    static public String test_token = "fake-token";
+    @BeforeAll
+    static public void beforeAll() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+        JedisConnectionFactory factory = new JedisConnectionFactory(config);
+        StringRedisTemplate template = new StringRedisTemplate(factory);
+        for (String key : template.keys("*"))
+            template.delete(key);
+        RedisTokenStore store = new RedisTokenStore(factory);
+        OAuth2AccessToken token = new DefaultOAuth2AccessToken(test_token);
+        User user = new User();
+        user.setId((int) 1L);
+        user.setUsername("bill");
+        user.setPassword("1234");
+        user.setEmail("bill@example.com");
+        user.setAdmin(false);
+        Authentication userAuth = new MyAuthentication(user);
+        OAuth2Request request = new OAuth2Request(new HashMap<>(), "client", new HashSet<>(),
+                true, new HashSet<>(), new HashSet<>(), "", new HashSet<>(), new HashMap<>());
+        OAuth2Authentication oauth = new OAuth2Authentication(request, userAuth);
+        TokenEnhancer enhancer = new UrlShortenerTokenEnhancer();
+        token = enhancer.enhance(token, oauth);
+        store.storeAccessToken(token, oauth);
+    }
     @BeforeEach
     void setUp() {
     }
@@ -43,44 +78,34 @@ class UserUrlControllerTest extends AuthTokenTests {
     @Test
     @DisplayName("shouldSuccessWhenRightInput")
     void findAllMyShortUrls() throws Exception {
-        int page=1;
-        int size=1;
         int userId=1;
         String testInfo="test info";
-        when( userUrlService.findAllMyShortUrls(page,size)).thenReturn(new Message(testInfo,null));
+        when( userUrlService.findAllMyShortUrls()).thenReturn(new Message(testInfo,null));
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/urlmanage/findurl")
-                .param("page",String.valueOf(page))
-                .param("size",String.valueOf(size))
                 .param("userId",String.valueOf(userId))
-                .header("Authorization", "Bearer " +test_token)
+                .header("Authorization", "Bearer " + test_token)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("status").value(testInfo))
                 .andDo(MockMvcResultHandlers.print())
-
                 .andReturn();
-      //  System.out.println(result);
     }
 
     @Test
     @DisplayName("shouldSuccessWhenRightInput")
     void deleteMyShortUrlById() throws Exception {
-
         int id=1;
-        int userId=1;
         String testInfo="test info";
         when( userUrlService.deleteMyShortUrlById(id)).thenReturn(new Message(testInfo,null));
-
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/urlmanage/deleteurl")
                 .param("id",String.valueOf(id))
-                .param("userId",String.valueOf(userId))
+                .header("Authorization", "Bearer " + test_token)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("status").value(testInfo))
                 .andDo(MockMvcResultHandlers.print())
-
                 .andReturn();
     }
 }
