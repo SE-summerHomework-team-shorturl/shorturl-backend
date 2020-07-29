ff852187682ae0d441d64614668fe082b276101c
billsjchw <billsjc@126.com>
2020-07-29 11:41:32 +0800

    feat: add prometheus to the POM files (spring-cloud-nosql)

--- shorturl-backend-spring-cloud-nosql/short-url-auth/src/main/java/com/example/shorturl/auth/config/WebSecurityConfig.java
+++ shorturl-backend-spring-cloud-nosql/short-url-auth/src/main/java/com/example/shorturl/auth/config/WebSecurityConfig.java
@@ -3,10 +3,16 @@ package com.example.shorturl.auth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().mvcMatchers("/actuator/**").permitAll();
+    }
+
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
